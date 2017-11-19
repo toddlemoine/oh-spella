f66ce2c37ef212b44536fbbd44acc8e412013328
Todd Lemoine <tlemoine@jamasoftware.com>
2017-11-19 08:04:32 -0800

    WIP: Sort of better, but broken
    Inner observables aren't unsubscribed from, which makes moving to next word impossible

--- src/game/gameState.js
+++ src/game/gameState.js
@@ -11,14 +11,19 @@ import {
   updateSkipStats,
   createStats
 } from "./stats";
+import { Subject } from "rxjs/Subject";
+
+const [COMPLETE, SKIPPED, MISSED] = [1, 2, 3];
 
 const initialState = {
   id: null,
-  wordList: [],
   currentWord: "",
   userWord: "",
-  complete: false,
-  finished: false,
+  wordList: [],
+  word: {
+    result: null, // complete, skipped, missed
+    correctHistory: []
+  },
   stats: []
 };
 
@@ -28,101 +33,6 @@ function isValidKeyEvent(e) {
   return e.which >= A_CHARCODE && e.which <= Z_CHARCODE;
 }
 
-function validGuess(guess, word) {
-  const isValid = word.startsWith(guess);
-  return isValid;
-}
-
-function action(type, attrs) {
-  return { type, ...attrs };
-}
-
-function getCompletePhrase() {
-  const phrases = `That's great!;Well done!;Keep going!;Excellent!`.split(";");
-  const randomNumber = Math.floor(Math.random() * (phrases.length - 1));
-  return phrases[randomNumber];
-}
-
-function validateUserWord({ text }, state) {
-  const attempt = state.userWord + text;
-  const isValid = validGuess(attempt, state.currentWord);
-  const userWord = isValid ? attempt : state.userWord;
-  updateLetterStats(text, state);
-  updateCorrectnessStats(isValid, state);
-
-  const complete = attempt === state.currentWord;
-  updateCompleteStats(complete, state);
-
-  const response = Observable.of({ ...state, userWord, complete });
-
-  response.subscribe(state => {
-    if (isValid) say(text);
-  });
-
-  return response;
-}
-
-function validateComplete({ text }, state) {
-  const complete = state.userWord + text === state.currentWord;
-  updateCompleteStats(complete, state);
-  return Observable.of({ complete });
-}
-
-function repeatWord(action, state) {
-  say(state.currentWord);
-  return Observable.of(state);
-}
-
-function resetState(state) {
-  return { ...state, id: uniqueId(), userWord: "", complete: false };
-}
-
-function nextWord(state) {
-  const { wordList, stats } = state;
-  const currentWord = wordList[stats.length];
-  state.currentWord = currentWord;
-  stats.push(createStats(currentWord));
-  return state;
-}
-
-function handleNextWord(_, state) {
-  console.log("handleNextWord");
-  const nextState = nextWord(resetState(state));
-  const response = Observable.of(nextState);
-  response.subscribe(({ currentWord }) => say(`Spell: ${currentWord}`));
-  return response;
-}
-
-function initialAnnouncement(action, state) {
-  return Observable.of(say(`Get ready!`)).mapTo(state);
-}
-
-function usedAllWords({ stats, wordList }) {
-  return stats.length === wordList.length;
-}
-
-function skipWord(_, state) {
-  updateSkipStats(state);
-  return usedAllWords(state)
-    ? Observable.of({ complete: true })
-    : handleNextWord(_, state);
-}
-
-function saveStats(state) {
-  return storage.setItem(state.id, state.stats);
-}
-
-function roundIsFinished(state) {
-  const { stats, wordList } = state;
-  const last = stats[stats.length - 1];
-  const isFinished = state.complete && usedAllWords(state) && last.end !== null;
-  return isFinished;
-}
-
-function initializeWords(action, state) {
-  return handleNextWord(null, { ...state, wordList: shuffle(action.wordList) });
-}
-
 function loadWordList(listParam) {
   const [namespace, listId] = listParam.split(":");
   return storage.getItem(namespace).then(lists => {
@@ -130,82 +40,55 @@ function loadWordList(listParam) {
   });
 }
 
-function checkIfRoundIsFinished(state) {
-  state.finished = roundIsFinished(state);
-  return state;
-}
-
 export function initialize(node, wordListId) {
-  const loadWords$ = Observable.fromPromise(loadWordList(wordListId));
-
-  // const clicks$ = Observable.fromEvent(node, "click")
-  //   .filter(e => /button/gi.test(e.target.nodeName))
-  //   .pluck("target", "id")
-  //   .map(action);
-
   const startSpeech$ = Observable.fromPromise(say("Get ready!"));
 
-  function createWordStream() {
-    return Observable.fromEvent(document, "keypress")
-      .filter(isValidKeyEvent)
-      .pluck("key");
-  }
+  const gameState$ = new BehaviorSubject(initialState);
 
-  const gameState$ = new BehaviorSubject({});
+  // Load words and set on state
+  Observable.fromPromise(loadWordList(wordListId))
+    .map(shuffle)
+    .subscribe(wordList => {
+      const currentWord = wordList.pop();
+      gameState$.next({ wordList, currentWord });
+    });
 
-  const wordList$ = loadWords$;
+  // Supporting streams
+  const letterPress$ = Observable.fromEvent(document, "keypress")
+    .filter(isValidKeyEvent)
+    .pluck("key");
 
-  const state$ = wordList$
-    .filter(wordList => Boolean(wordList.length))
-    .map(wordList => ({
-      ...initialState,
-      wordList,
-      currentWord: wordList[0]
-    }));
-
-  state$.subscribe(state => gameState$.next(state));
-
-  const wordStateStream = gameState$
+  const round$ = gameState$
     .skip(1)
-    .distinct(state => state.currentWord)
-    .do(state => say(`Spell ${state.currentWord}`))
-    .concatMap(state => {
-      // .switchMap(state => {
+    // .takeWhile(state => state.wordList.length)
+    .distinctUntilKeyChanged("currentWord")
+    .do(({ currentWord }) => say(`Spell: ${currentWord}`))
+    .switchMap(state => {
+      console.log("-------------- switchMap -----", state.currentWord);
       const wordToSpell = state.currentWord;
-      const word$ = createWordStream();
 
-      const attempt$ = word$
+      const attempt$ = letterPress$
         .scan((userWord, key) => {
           const attempt = userWord + key;
           return wordToSpell.startsWith(attempt) ? attempt : userWord;
         })
-        .do(x => console.log("attempt", x))
-        .takeWhile(userWord => userWord !== wordToSpell);
-
-      attempt$.subscribe({
-        complete: () => console.log("done!")
-      });
+        .do(x => console.log("attempt", x));
 
       // userWord state
-      const userWord$ = word$
-        .scan((userWord, key) => {
-          const attempt = userWord + key;
-          return wordToSpell.startsWith(attempt) ? attempt : userWord;
-        })
-        .distinct()
-        .map(userWord => ({ userWord }));
-
-      // Complete state
-      const complete$ = userWord$
-        .pluck("userWord")
-        .filter(userWord => userWord === wordToSpell)
-        .switchMap(() => {
-          return Observable.of({ complete: true }).delay(3000);
-        });
-
+      const userWord$ = attempt$.distinct().map(userWord => ({ userWord }));
       userWord$.subscribe(({ userWord }) => say(userWord.substr(-1)));
 
-      const correctHistory$ = word$
+      // Complete state
+      const complete$ = attempt$
+        .filter(userWord => userWord === wordToSpell)
+        .mapTo(true);
+
+      complete$.subscribe(() => {
+        say("correct");
+      });
+
+      // correctHistory state
+      const correctHistory$ = letterPress$
         .withLatestFrom(userWord$)
         .map(([key, { userWord }]) => userWord.endsWith(key))
         .scan(
@@ -214,10 +97,38 @@ export function initialize(node, wordListId) {
         )
         .map(history => ({ history }));
 
-      return Observable.merge(userWord$, correctHistory$, complete$);
+      const merged = Observable.merge(userWord$, correctHistory$)
+        .takeUntil(complete$)
+        .scan((acc, curr) => ({ ...acc, ...curr }), state);
+
+      merged.subscribe({
+        complete: () => {
+          console.log("merged complete, unsubscribing");
+          const currentWord = state.wordList.pop();
+          console.log("next word", currentWord, state.wordList);
+          const nextState = {
+            wordList: state.wordList,
+            currentWord,
+            complete: false,
+            history: [],
+            userWord: ""
+          };
+
+          Observable.of(nextState)
+            .delay(3000)
+            .subscribe(nextState => gameState$.next(nextState));
+        }
+      });
+
+      return merged;
     });
 
-  return Observable.merge(state$, wordStateStream)
-    .scan((acc, curr) => ({ ...acc, ...curr }))
-    .do(x => console.log("merged", x));
+  round$.subscribe({
+    next: state => gameState$.next(state),
+    complete: () => {
+      console.log("Round complete");
+    }
+  });
+
+  return gameState$;
 }

a176564fb08436b1b136eff4185d8b24a64c8a11
Todd Lemoine <tlemoine@jamasoftware.com>
2017-12-08 04:59:46 -0800

    Use interval to check for speech end
    msg.onend wasn't reliable.

--- src/speech.js
+++ src/speech.js
@@ -2,10 +2,22 @@ export const say = (text, options = {}) => {
   return new Promise(resolve => {
     const synth = window.speechSynthesis;
     const msg = new SpeechSynthesisUtterance(text);
-    msg.onend = () => resolve(text);
+
     for (let [key, val] of Object.entries(options)) {
       msg[key] = val;
     }
+
+    msg.onerror = err => console.error("speech error", err);
+
     synth.speak(msg);
+
+    function checkIfSpeaking() {
+      if (synth.speaking) {
+        resolve(text);
+        clearInterval(speechCheckIntervalId);
+      }
+    }
+
+    const speechCheckIntervalId = setInterval(checkIfSpeaking, 250);
   });
 };
